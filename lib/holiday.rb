1d2c11f61a9086db6fef4f689ef59a8e9ec10d5e
Caio Sanches <ccampato@gmail.com>
2018-04-26 03:17:12 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,9 +62,9 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |name, supply|
       name_c = name.to_s
       if name_c.include?("_")
-        array = name_c.split
-        array.each { |i| i.capitalize}
-        name_c = array.join(" ")
+        ar = name_c.split("_")
+        ar_c = ar.each {|i| i.capitalize}
+        name_c= ar_c.join(" ")
       else
         name_c = name.capitalize
       end
