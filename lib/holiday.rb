8f3900c42351cfe1d5b84f0130b6a9374719c739
Caio Sanches <ccampato@gmail.com>
2018-04-26 02:55:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts  "#{season}:".capitalize
     holiday.each do |name, supply|
-      puts " #{name}".capitalize
+      name_c = name.capitalize
+      puts " #{name_c}:"
     end
   end
 end
